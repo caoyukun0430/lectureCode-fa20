64a524ee922b97f4443981f4ed3b2d5d68696cb4
Yukun Cao <caoyukunk9999@126.com>
2021-03-16 22:54:22 +0100

    Add comments for ex4 HW0

--- intro1/HW0/BreakContinue.java
+++ intro1/HW0/BreakContinue.java
@@ -1,16 +1,21 @@
 public class BreakContinue {
+  /**Optional: Exercise 4
+   * replaces each element a[i] with the sum of a[i] through a[i + n], but only if a[i] is positive valued.
+   */
   public static void windowPosSum(int[] a, int n) {
+    // Deepcopy of a, otherwise will be modify
     int [] b = java.util.Arrays.copyOf(a, a.length);
-    System.out.println(java.util.Arrays.toString(a));
     for (int i = 0; i < a.length; i++) {
         if (a[i] < 0) {
           continue;
         }
+        // define the upper bound when i+n exceeds
         int upper = i+n <= a.length-1 ? i + n : a.length-1;
         for (int j = i; j <= upper; j++) {
           System.out.printf("i is %d, j is %d, aj is %d \n", i, j, a[j]);
           a[i] += a[j];
         }
+        // a[i] has added itself twice
         a[i] = a[i] - b[i];
     }
   }
